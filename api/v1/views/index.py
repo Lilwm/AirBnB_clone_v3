0bb8a3b60008facad3063b51bb60452f7f4a8ad3
lilwm <lmiiri95@gmail.com>
2023-02-22 07:27:36 +0000

    fixed shebang + Pep8 validation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python3
+"""API module"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'])
 def get_status():
+    """checks connection status"""
     return jsonify({'status': 'OK'})
+
 
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
